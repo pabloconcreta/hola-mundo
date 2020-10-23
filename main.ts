0f23a88d4c7cd47a2e502c24bb73f8805c376c9b
pabloconcreta <pablo@concreta.com.uy>
2020-10-23 00:23:20 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onPinPressed(TouchPin.P0, function () {
+    music.playMelody("C5 - C5 - C5 - C5 - ", 140)
+})
+
+input.onButtonPressed(Button.A, function () {
+	music.playMelody("C5 D5 E5 F5 G5 A5 B5 C6", 40)
+})
